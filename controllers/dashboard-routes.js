1fb2146b0d581044e7449e8520d5905bb2c98fd4
Dylan Barstow <dcbarstow7@gmail.com>
2021-10-04 17:54:28 -0500

    testing tab

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -6,88 +6,88 @@ const withAuth = require("../utils/auth");
 
 // GET ALL POSTS OF LOGGED IN(withAuth) USER
 router.get("/", withAuth, (req, res) => {
-		Post.findAll({
-				where: {
-						user_id: req.session.user_id,
-				},
-				attributes: ["id", "title", "content", "created_at"],
-				include: [
-						{
-								model: Comment,
-								attributes: [
-										"id",
-										"comment_text",
-										"post_id",
-										"user_id",
-										"created_at",
-								],
-								include: {
-										model: User,
-										attributes: ["username"],
-								},
-						},
-						{
-								model: User,
-								attributes: ["username"],
-						},
-				],
-		})
-				.then((dbPostData) => {
-						const posts = dbPostData.map((post) => post.get({ plain: true }));
-						res.render("dashboard", { posts, loggedIn: true });
-				})
-				.catch((err) => {
-						console.log(err);
-						res.status(500).json(err);
-				});
+  Post.findAll({
+    where: {
+      user_id: req.session.user_id,
+    },
+    attributes: ["id", "title", "content", "created_at"],
+    include: [
+    {
+      model: Comment,
+        attributes: [
+          "id",
+          "comment_text",
+          "post_id",
+          "user_id",
+          "created_at",
+        ],
+        include: {
+          model: User,
+            attributes: ["username"],
+        },
+        },
+        {
+          model: User,
+          attributes: ["username"],
+            },
+        ],
+    })
+        .then((dbPostData) => {
+            const posts = dbPostData.map((post) => post.get({ plain: true }));
+            res.render("dashboard", { posts, loggedIn: true });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 // GET ONE POST OF LOGGED IN(withAuth) USER
 router.get("/edit/:id", withAuth, (req, res) => {
-		Post.findOne({
-				where: {
-						id: req.params.id,
-				},
-				attributes: ["id", "title", "content", "created_at"],
-				include: [
-						{
-								model: User,
-								attributes: ["username"],
-						},
-						{
-								model: Comment,
-								attributes: [
-										"id",
-										"comment_text",
-										"post_id",
-										"user_id",
-										"created_at",
-								],
-								include: {
-										model: User,
-										attributes: ["username"],
-								},
-						},
-				],
-		})
-				.then((dbPostData) => {
-						if (!dbPostData) {
-								res.status(404).json({ message: "No post found with this id" });
-								return;
-						}
+    Post.findOne({
+        where: {
+            id: req.params.id,
+        },
+        attributes: ["id", "title", "content", "created_at"],
+        include: [
+            {
+                model: User,
+                attributes: ["username"],
+            },
+            {
+                model: Comment,
+                attributes: [
+                    "id",
+                    "comment_text",
+                    "post_id",
+                    "user_id",
+                    "created_at",
+                ],
+                include: {
+                    model: User,
+                    attributes: ["username"],
+                },
+            },
+        ],
+    })
+        .then((dbPostData) => {
+            if (!dbPostData) {
+                res.status(404).json({ message: "No post found with this id" });
+                return;
+            }
 
-						const post = dbPostData.get({ plain: true });
-						res.render("edit-post", { post, loggedIn: true });
-				})
-				.catch((err) => {
-						console.log(err);
-						res.status(500).json(err);
-				});
+            const post = dbPostData.get({ plain: true });
+            res.render("edit-post", { post, loggedIn: true });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 // CREATE A NEW POST
 router.get("/new", (req, res) => {
-		res.render("new-post");
+    res.render("new-post");
 });
 
 module.exports = router;
