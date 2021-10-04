bab9becd2b63d6cb149e1389f2e40e332c19beed
Dylan Barstow <dcbarstow7@gmail.com>
2021-10-04 17:59:03 -0500

    why dot they look so goofy on github

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -12,7 +12,7 @@ router.get("/", withAuth, (req, res) => {
     },
     attributes: ["id", "title", "content", "created_at"],
     include: [
-    {
+      {
       model: Comment,
         attributes: [
           "id",
@@ -28,66 +28,65 @@ router.get("/", withAuth, (req, res) => {
         },
         {
           model: User,
-          attributes: ["username"],
-            },
-        ],
-    })
-        .then((dbPostData) => {
-            const posts = dbPostData.map((post) => post.get({ plain: true }));
-            res.render("dashboard", { posts, loggedIn: true });
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(500).json(err);
+            attributes: ["username"],
+        },
+    ],
+  })
+  .then((dbPostData) => {
+    const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("dashboard", { posts, loggedIn: true });
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
         });
 });
 
 // GET ONE POST OF LOGGED IN(withAuth) USER
 router.get("/edit/:id", withAuth, (req, res) => {
-    Post.findOne({
-        where: {
-            id: req.params.id,
-        },
-        attributes: ["id", "title", "content", "created_at"],
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+      attributes: ["id", "title", "content", "created_at"],
         include: [
-            {
-                model: User,
+          {
+            model: User,
+              attributes: ["username"],
+          },
+          {
+            model: Comment,
+              attributes: [
+                  "id",
+                  "comment_text",
+                  "post_id",
+                  "user_id",
+                  "created_at",
+              ],
+            include: {
+              model: User,
                 attributes: ["username"],
             },
-            {
-                model: Comment,
-                attributes: [
-                    "id",
-                    "comment_text",
-                    "post_id",
-                    "user_id",
-                    "created_at",
-                ],
-                include: {
-                    model: User,
-                    attributes: ["username"],
-                },
-            },
-        ],
-    })
-        .then((dbPostData) => {
-            if (!dbPostData) {
-                res.status(404).json({ message: "No post found with this id" });
-                return;
-            }
-
-            const post = dbPostData.get({ plain: true });
-            res.render("edit-post", { post, loggedIn: true });
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+          },
+      ],
+  })
+  .then((dbPostData) => {
+      if (!dbPostData) {
+          res.status(404).json({ message: "No post found with this id" });
+      return;
+      }
+        const post = dbPostData.get({ plain: true });
+          res.render("edit-post", { post, loggedIn: true });
+  })
+  .catch((err) => {
+    console.log(err);
+      res.status(500).json(err);
+  });
 });
 
 // CREATE A NEW POST
 router.get("/new", (req, res) => {
-    res.render("new-post");
+  res.render("new-post");
 });
 
 module.exports = router;
