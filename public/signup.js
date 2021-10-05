01a8d7d996e7089473feb3213d2317a3d6d76099
Dylan Barstow <dcbarstow7@gmail.com>
2021-10-04 19:02:29 -0500

    email signup value added

--- public/signup.js
+++ public/signup.js
@@ -1,27 +1,24 @@
 const signupFormHandler = async (event) => {
     event.preventDefault();
 
-    // USERNAME AND PASSWORD VARIABLES
     const name = document.querySelector('#name-signup').value.trim();
-    const username = document.querySelector("#username-signup").value.trim();
-    const password = document.querySelector("#password-signup").value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
 
-    // IF BOTH ARE PRESENT RUN THIS
-    if (name && username && password) {
-        const response = await fetch("/api/users", {
-            method: "POST",
-            body: JSON.stringify({  username, password  }),
-            headers: { "Content-Type": "application/json" },
-        });
-        if (response.ok) {
-            console.log("success");
-            document.location.replace("/");
-        } else {
-            alert(response.statusText);
-        }
+    if (name && email && password) {
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({ name, email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert(response.statusText);
+      }
     }
-}
+  };
 
-document
-    .querySelector("#signup-form")
-    .addEventListener("submit", signupFormHandler);
+  document
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupFormHandler);
