3e6d856edcb29e341ce08233e59443d23096b527
Dylan Barstow <dcbarstow7@gmail.com>
2021-10-04 19:04:04 -0500

    switching to email login

--- public/login.js
+++ public/login.js
@@ -2,25 +2,25 @@ const loginFormHandler = async (event) => {
     event.preventDefault();
 
     // Collect values from the login form
-    const username = document.querySelector("#username-login").value.trim();
-    const password = document.querySelector("#password-login").value.trim();
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
 
-    // Send a POST request to the API endpoint
-    if (username && password) {
-        const response = await fetch("/api/users/login", {
-            method: "POST",
-            body: JSON.stringify({ username, password }),
-            headers: { "Content-Type": "application/json" },
-        });
-        if (response.ok) {
-            // If successful, redirect the browser to the profile page
-            document.location.replace("/");
-        } else {
-            alert(response.statusText);
-        }
+    if (email && password) {
+      // Send a POST request to the API endpoint
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+      if (response.ok) {
+        // If successful, redirect the browser to the profile page
+        document.location.replace('/');
+  2    } else {
+        alert(response.statusText);
+      }
     }
-}
+  };
 
-document
-    .querySelector("#login-form")
-    .addEventListener("submit", loginFormHandler);
+  document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
