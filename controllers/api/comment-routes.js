081ed277d319ed69e545af217491807680e42247
Dylan Barstow <dcbarstow7@gmail.com>
2021-10-04 19:27:51 -0500

    updating routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
-const { Comment } = require("../models");
-const withAuth = require("../utils/auth");
+const { Comment } = require("../../models");
+const withAuth = require("../../utils/auth");
 
 // CREATES/POSTS A NEW COMMENT
 router.post('/', withAuth, async (req, res) => {
