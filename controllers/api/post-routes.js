a1c10eeabeea94cb8f94e5b6926375df350a27be
Dylan Barstow <dcbarstow7@gmail.com>
2021-10-04 19:01:09 -0500

    updating routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const { Post} = require('../../models');
-const withAuth = require('../../utils/auth');
+const { Post} = require('../models');
+const withAuth = require('../utils/auth');
 
   // find all posts
 router.get('/', async (req, res) => {
