bf863a34fbc5aa9a35a5b9b3f7b3dca27602e549
Dylan Barstow <dcbarstow7@gmail.com>
2021-10-13 17:45:29 -0500

    more helpers updates

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,14 @@
 module.exports = {
-    format_date: date => {
-        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
+    format_date: (date) => {
+        return date.toLocalTimeString();
     },
-}
+    
+    format_date: (date) => {
+      return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
+    },
+    
+    // checks if the author is the signed in user
+    isAuthor: (myId, authorId) => {
+      return myId === authorId;
+  }
+};
