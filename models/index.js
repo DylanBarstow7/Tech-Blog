8848cb0de9a09713f194097478a0dbe30db59cc8
Dylan Barstow <dcbarstow7@gmail.com>
2021-10-13 17:43:56 -0500

    onDelete updates

--- models/index.js
+++ models/index.js
@@ -1,34 +1,29 @@
-const User = require("./User");
-const Post = require("./Post");
-const Comment = require("./Comment");
+const User = require("./User.js");
+const Post = require("./Post.js");
+const Comment = require("./Comment.js");
 
 User.hasMany(Post, {
-    foreignKey: "user_id",
+  foreignKey: "user_id"
 });
 
 Post.belongsTo(User, {
-    foreignKey: "user_id",
-    onDelete: "cascade",
-});
-
-User.hasMany(Comment, {
-    foreignKey: "user_id",
-    onDelete: "cascade",
-});
-
-Comment.belongsTo(User, {
-    foreignKey: "user_id",
-    onDelete: "cascade",
+  foreignKey: "user_id",
+  onDelete: "CASCADE"
 });
 
 Post.hasMany(Comment, {
-    foreignKey: "post_id",
-    onDelete: "cascade",
+  foreignKey: "post_id",
+  onDelete: "CASCADE"
 });
 
 Comment.belongsTo(Post, {
-    foreignKey: "post_id",
-    onDelete: "cascade",
+  foreignKey: "post_id",
+  onDelete: "CASCADE"
 });
 
-module.exports = { User, Post, Comment };
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE"
+});
+
+module.exports = { User, Post, Comment};
