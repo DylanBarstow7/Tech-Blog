6fe5cefb9cd4c826bae4fc804162c582d6937bf5
Dylan Barstow <dcbarstow7@gmail.com>
2021-10-13 20:12:38 -0500

    home route edit + router.get dashboard by id

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,23 +1,23 @@
-const router = require('express').Router();
-const { Post, User, Comment } = require('../models');
+const router = require("express").Router();
+const { Post, User, Comment } = require("../models");
+const auth = require("../utils/auth");
 
-// When the homepage is loaded, the post table will cycle through and display the post data and each post's user data.
-router.get('/', async (req, res) => {
+// When the homepage is loaded, it will display the post data and each post"s user data.
+router.get("/", async (req, res) => {
   try {
     // Get all posts and JOIN with user data
     const postData = await Post.findAll({
       include: [
         {
           model: User,
-          attributes: ['username'],
+          attributes: ["username"],
         },
-        { model: Comment }
       ],
     });
     // Serialize data so the template can read it
     const posts = postData.map(post => post.get({ plain: true }));
     // Pass serialized data and session flag into template
-    res.render('homepage', {
+    res.render("homepage", {
       posts,
       loggedIn: req.session.loggedIn
     });
@@ -26,22 +26,26 @@ router.get('/', async (req, res) => {
   }
 });
 
-// Then the post is called on the 'post' page, the post will be found based on the given ID, the data will be gathered including the user name and date. The comments associated with that post will also be displayed including the associated user and date of the comment.
-router.get('/post/:id', async (req, res) => {
+// Then the post is called on the "post" page, the post will be found based on the given ID, the data will be gathered including the user name and date. The comments associated with that post will also be displayed including the associated user and date of the comment.
+router.get("/post/:id", async (req, res) => {
   try {
     const postData = await Post.findByPk(req.params.id, {
       include: [
         {
           model: User,
+          attributes: ["username"],
         },
         {
           model: Comment,
-          include: [
-            {
+          attributes: [
+            "title", 
+            "description", 
+            "date_created"
+          ],
+          include: [{
             model: User, 
             attributes:["username"]
-            }
-          ]
+          }]
         },
       ],
     });
@@ -50,14 +54,9 @@ router.get('/post/:id', async (req, res) => {
 
     console.log(post);
 
-    let owner = false;
-    if(req.session.user_id === post.user_id){
-        owner = true;
-    };
-    res.render('post', {
-        post,
-        loggedIn: req.session.loggedIn,
-        owner,
+    res.render("post", {
+        ...post,
+        loggedIn: req.session.loggedIn
     });
   } catch (err) {
     console.log(err);
@@ -65,47 +64,71 @@ router.get('/post/:id', async (req, res) => {
   }
 });
 
-// --------------------------------------------------------------------
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
-      res.redirect('/');
+      res.redirect("/dashboard");
       console.log(req.session.loggedIn);
       return;
   }
-  // Otherwise, render the 'login' template
-  res.render('login');
+  // Otherwise, render the "login" template
+  res.render("login");
 });
 
-// --------------------------------------------------------------------
-router.get('/dashboard', async (req, res) => {
-  try {
+router.get("/signup", (req,res) => {
+  res.render("signup");
+});
 
+router.get("/dashboard", auth, async (req, res) => {
+  try {
       const userData = await User.findByPk(req.session.user_id, {
-          attributes: { exclude: ['password'] },
+          attributes: { exclude: ["password"] },
           include: [{ model: Post }],
       });
 
       const user = userData.get({ plain: true });
 
-      res.render('dashboard', {
-          user,
-          loggedIn: req.session.loggedIn,
-          // loggedIn: true,
+      res.render("dashboard", {
+        ...user,
+        loggedIn: true
       });
-
       console.log(user);
-
-  } catch (error) {
-      res.status(400).json(error);
+  } catch (err) {
+      res.status(500).json(err);
   }
-})
+});
 
-router.get('/signup', (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect('/');
-    return;
+router.get("/dashboard/post/:id", auth, async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+    include: [
+      {
+        model: User,
+        attributes: ["username"],
+      },
+      {
+        model: Comment,
+        attributes: [
+          "title", 
+          "description", 
+          "date_created"
+        ],
+        include: [{
+          model: User, 
+          attributes:["username"]
+        }],
+      },
+    ],
+  }); 
+
+  const post = postData.get({ plain: true });
+  
+  res.render("editpost", {
+    ...post,
+    loggedIn: req.session.loggedIn
+  });
+  } catch (err) {
+  res.status(500).json(err);
   }
-  res.render('signup');
 });
 
 module.exports = router;
